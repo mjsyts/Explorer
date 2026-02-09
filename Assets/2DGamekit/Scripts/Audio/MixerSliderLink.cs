6a67053de9bdd1dd7e52daefba10fda80f21cc87
Josiah Sytsma <77947318+mjsyts@users.noreply.github.com>
2026-02-09 06:14:16 +0000

    Refactor MixerSliderLink to integrate Wwise RTPC for audio control

--- Assets/2DGamekit/Scripts/Audio/MixerSliderLink.cs
+++ Assets/2DGamekit/Scripts/Audio/MixerSliderLink.cs
@@ -2,38 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Audio;
 
 namespace Gamekit2D
 {
     [RequireComponent(typeof(Slider))]
-    public class MixerSliderLink : MonoBehaviour
+    public class WwiseMixerSliderLink : MonoBehaviour
     {
-        public AudioMixer mixer;
-        public string mixerParameter;
+        public AK.Wwise.RTPC rtpcParameter;
 
-        public float maxAttenuation = 0.0f;
-        public float minAttenuation = -80.0f;
+        public float maxValue = 100.0f;
+        public float minValue = 0.0f;
 
         protected Slider m_Slider;
 
-
-        void Awake ()
+        void Awake()
         {
             m_Slider = GetComponent<Slider>();
 
-            float value;
-            mixer.GetFloat(mixerParameter, out value);
-
-            m_Slider.value = (value - minAttenuation) / (maxAttenuation - minAttenuation);
+            // Get current RTPC value
+            float currentValue = rtpcParameter.GetGlobalValue();
+            
+            // Normalize to 0-1 for slider
+            m_Slider.value = (currentValue - minValue) / (maxValue - minValue);
 
             m_Slider.onValueChanged.AddListener(SliderValueChange);
         }
 
-
-        void SliderValueChange(float value)
+        void SliderValueChange(float normalizedValue)
         {
-            mixer.SetFloat(mixerParameter, minAttenuation + value * (maxAttenuation - minAttenuation));
+            // Convert slider value (0-1) to RTPC range
+            float rtpcValue = minValue + normalizedValue * (maxValue - minValue);
+            
+            // Set global RTPC value
+            rtpcParameter.SetGlobalValue(rtpcValue);
         }
     }
 }
--- docs/mixer-slider.md
+++ docs/mixer-slider.md
@@ -4,8 +4,47 @@
 
 ## Implementation
 
-We'll add an RTPC member variable to the class:
+First, we'll replace the Unity `AudioMixer` with `AK.Wwise.RTPC`:
 
 ```cs
     public AK.Wwise.RTPC rtpcParameter;
+```
+
+Unity used attenuation values in dB, but RTPCs in Wwise default to a range of 0-100. You *can* change the RTPCs in Wwise to mimic the original Unity implementation, but Wwise is my Single Source Of Truth for audio in this project. I changed the member variable names to something more generalized, so if you want to stay more faithful to the Unity implementation you'll need to keep this in mind:
+
+```cs
+        public float maxValue = 100.0f;
+        public float minValue = 0.0f;
+```
+
+The internal logic is the same for the rest of the class: `Slider` should fetch the current internal mix value it represents on `Awake()` and set the value on `SliderValueChange()`.
+
+### `Awake()`
+
+```cs
+        void Awake()
+        {
+            m_Slider = GetComponent<Slider>();
+
+            // Get current RTPC value
+            float currentValue = rtpcParameter.GetGlobalValue();
+            
+            // Normalize to 0-1 for slider
+            m_Slider.value = (currentValue - minValue) / (maxValue - minValue);
+
+            m_Slider.onValueChanged.AddListener(SliderValueChange);
+        }
+```
+
+### `SliderChangeValue(...)`
+
+```cs
+        void SliderValueChange(float normalizedValue)
+        {
+            // Convert slider value (0-1) to RTPC range
+            float rtpcValue = minValue + normalizedValue * (maxValue - minValue);
+            
+            // Set global RTPC value
+            rtpcParameter.SetGlobalValue(rtpcValue);
+        }
 ```
